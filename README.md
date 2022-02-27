2762779872ac2a26528bced97eb9f1697c627a74
Stanko <stanko.tadic@gmail.com>
2022-02-27 16:02:17 +0100

    Mounting bracket preview. nvmrc.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# My JSCAD Models
+# My JSCAD parametric 3d models
 
-https://github.com/jscad/OpenJSCAD.org
+I started learning to model using [JSCAD](https://github.com/jscad/OpenJSCAD.org). This repo will contain my models with direct links for editing and STL file download.
+
+### Mounting bracket
+
+[![](/mounting-bracket/preview.png)](https://www.openjscad.xyz/?uri=https://raw.githubusercontent.com/Stanko/jscad-models/dev/mounting-bracket/index.js)
+
+[Edit and download on openjscad.xyz](https://www.openjscad.xyz/?uri=https://raw.githubusercontent.com/Stanko/jscad-models/dev/mounting-bracket/index.js)
--- mounting-bracket/index.js
+++ mounting-bracket/index.js
@@ -1,94 +1,52 @@
-const { cuboid, cylinderElliptic } = require('@jscad/modeling').primitives
-const { union, subtract } = require('@jscad/modeling').booleans
- 
-const main = (params) => {
-  const object = cuboid({
-    size: [100, params.innerWidth, params.innerHeight],
-  });
+const { cuboid, cylinderElliptic } = require('@jscad/modeling').primitives;
+const { union, subtract } = require('@jscad/modeling').booleans;
+const { colorize } = require('@jscad/modeling').colors;
+const { vectorText } = require('@jscad/modeling').text;
 
+const main = (params) => {
   const halfThickness = params.wallThickness / 2;
 
   const bottom = {
     depth: params.bracketWidth,
     width: params.innerWidth + 2 * params.wallThickness,
     height: params.wallThickness,
-  }
+  };
 
   const side = {
     depth: params.bracketWidth,
     width: params.wallThickness,
     height: params.innerHeight + 2 * params.wallThickness,
-  }
+  };
 
   const arm = {
-    depth: params.bracketWidth, 
-    width: (params.innerWidth / 100 * params.armsLength) + params.wallThickness, 
+    depth: params.bracketWidth,
+    width: (params.innerWidth / 100) * params.armsLength + params.wallThickness,
     height: params.wallThickness,
-  }
-  
+  };
+
   const walls = [
     // bottom
     cuboid({
-      size: [
-        bottom.depth, 
-        bottom.width,
-        bottom.height,
-      ],
-      center: [
-        0, 
-        0,
-        params.wallThickness / 2,
-      ]
+      size: [bottom.depth, bottom.width, bottom.height],
+      center: [0, 0, params.wallThickness / 2],
     }),
     // sides
     cuboid({
-      size: [
-        side.depth, 
-        side.width,
-        side.height,
-      ],
-      center: [
-        0, 
-        bottom.width / 2 - halfThickness,
-        side.height / 2,
-      ]
+      size: [side.depth, side.width, side.height],
+      center: [0, bottom.width / 2 - halfThickness, side.height / 2],
     }),
     cuboid({
-      size: [
-        side.depth, 
-        side.width,
-        side.height,
-      ],
-      center: [
-        0, 
-        bottom.width / -2 + halfThickness,
-        side.height / 2,
-      ]
+      size: [side.depth, side.width, side.height],
+      center: [0, bottom.width / -2 + halfThickness, side.height / 2],
     }),
     // top arms
     cuboid({
-      size: [
-        arm.depth, 
-        arm.width,
-        arm.height,
-      ],
-      center: [
-        0,
-        (bottom.width - arm.width) / 2,
-        side.height - halfThickness,
-      ],
+      size: [arm.depth, arm.width, arm.height],
+      center: [0, (bottom.width - arm.width) / 2, side.height - halfThickness],
     }),
     cuboid({
-      size: [
-        arm.depth, 
-        arm.width,
-        arm.height,
-      ],
-      center: [
-        0,
-        (bottom.width - arm.width) / -2,
-        side.height - halfThickness,
-      ],
+      size: [arm.depth, arm.width, arm.height],
+      center: [0, (bottom.width - arm.width) / -2, side.height - halfThickness],
     }),
   ];
 
@@ -101,35 +59,51 @@ const main = (params) => {
     height: params.wallThickness,
     endRadius: [endRadius, endRadius],
     startRadius: [startRadius, startRadius],
-    center: [
-      0,
-      0,
-      halfThickness
-    ]
+    center: [0, 0, halfThickness],
   });
 
   bracket = subtract(bracket, hole);
 
-
-  return [
-    // object,
-    bracket,
-  ]
-}
-
-
+  return [bracket];
+};
 
 const getParameterDefinitions = () => {
   return [
-    { name: 'innerWidth', type: 'float', initial: 62, caption: 'Inner length' }, 
+    { name: 'innerWidth', type: 'float', initial: 62, caption: 'Inner length' },
     { name: 'innerHeight', type: 'float', initial: 46, caption: 'Inner width' },
-    { name: 'wallThickness', type: 'float', initial: 4, caption: 'Wall thickness' },
-    { name: 'armsLength', type: 'float', initial: 30, caption: 'Arms length (10 - 40%)' },
-    { name: 'bracketWidth', type: 'float', initial: 30, caption: 'Bracket Width' },
-    { name: 'holeDiameterTop', type: 'float', initial: 12, caption: 'Hole top diameter' },
-    { name: 'holeDiameterBottom', type: 'float', initial: 6, caption: 'Hole bottom diameter' },
-  ]
-}
- 
-module.exports = { main, getParameterDefinitions }
+    {
+      name: 'wallThickness',
+      type: 'float',
+      initial: 4,
+      caption: 'Wall thickness',
+    },
+    {
+      name: 'armsLength',
+      type: 'slider',
+      initial: 30,
+      min: 10,
+      max: 45,
+      caption: 'Arms length (10 - 45%)',
+    },
+    {
+      name: 'bracketWidth',
+      type: 'float',
+      initial: 30,
+      caption: 'Bracket Width',
+    },
+    {
+      name: 'holeDiameterTop',
+      type: 'float',
+      initial: 12,
+      caption: 'Hole top diameter',
+    },
+    {
+      name: 'holeDiameterBottom',
+      type: 'float',
+      initial: 6,
+      caption: 'Hole bottom diameter',
+    },
+  ];
+};
 
+module.exports = { main, getParameterDefinitions };
